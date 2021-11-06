23762116d4ac3fde83532dc0d8a0964abbaa83d2
Peteris Gulbis <gulbis.storhs@gmail.com>
2021-11-06 15:52:18 +0200

    XunitTest

--- Calculator/calculatordemo.Test/calculator.cs
+++ Calculator/calculatordemo.Test/calculator.cs
@@ -3,12 +3,65 @@ using Xunit;
 
 namespace calculatordemo.Test
 {
-    public class UnitTest1
+    public class Calculator
     {
         [Fact]
-        public void Test1()
+        public void GetTwoNumbers_WhenNumbersSum_ThenReturnSum()
         {
+            //If two numbers sum, then return "sum"
+            //If two numbers subtrack, then return "minuss"
+            //If two numbers multiply, then return "multiply"
+            //If two numbers divide, then return "divide"
+
+            //Arrange
+            var calculator = new calculator();
+
+            //Act
+            var result = calculator.GetNumbers(1);
+            //Assert
+            Assert.Equal("1", result);
 
         }
+
+        [Fact]
+        public void GetTwoNumbers_WhenNumbersSubtrack_ThenReturnMinuss()
+        {
+            //Arrange
+            var calculator = new calculator();
+
+            //Act
+            var result = calculator.GetNumbers(10);
+            //Assert
+            Assert.Equal("minuss", result);
+
+        }
+        [Fact]
+        public void GetTwoNumbers_WhenNumbersMultiply_ThenReturnMultiply()
+        {
+            //Arrange
+            var calculator = new calculator();
+
+            //Act
+            var result = calculator.GetNumbers(0);
+            //Assert
+            Assert.Equal("multiply", result);
+
+        }
+        [Theory]
+        [InlineData(100)]
+        [InlineData(50)]
+        [InlineData(20)]
+        public void GetTwoNumbers_WhenNumbersDivide_ThenReturnDivide(int testableNumber)
+        {
+            //Arrange
+            var calculator = new calculator();
+
+            //Act
+            var result = calculator.GetNumbers(testableNumber);
+            //Assert
+            Assert.Equal("divide", result);
+
+        }
+
     }
 }
---
+++ Calculator/calculatordemo/calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatordemo
+{
+    public class calculator
+    {
+        public object GetNumbers(int number)
+        {
+
+            if (number - 10 == 0)
+            {
+                return "minuss";
+            }
+            if (number * 5 == 0) 
+            {
+                return "multiply";
+            }
+            if (number % 5 == 0) 
+            {
+                return "divide";
+            }
+            return number.ToString();
+        }
+
+        public object GetNumbers(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
